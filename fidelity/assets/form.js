1430f621a42205024a076cb80f7f80988a3d5b9f
Panda Bar | Lattefy <panda.lattefy@gmail.com>
2024-12-10 22:16:47 -0300

    9 --> 8 panditas

--- fidelity/assets/form.js
+++ fidelity/assets/form.js
@@ -329,7 +329,7 @@ document.addEventListener('DOMContentLoaded', async function () {
       const client = await getClientByPhoneNumber(phoneNumber)
       console.log(client)
       document.getElementById("client-name").innerHTML = client.name
-      document.getElementById("current-points").innerHTML = `${client.currentPoints} / 9`
+      document.getElementById("current-points").innerHTML = `${client.currentPoints} / 8`
     } else {
       window.location.href = './path.html'
       console.log('phoneNumber not found in the URL')
